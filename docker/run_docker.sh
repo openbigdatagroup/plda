4339845b390dd055ca91d19957bd92f8135e6979
Bo Wang(王勃) <bo_wang@htc.com>
2016-03-09 15:21:30 +0800

    remove dockers by name

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -19,11 +19,18 @@ for((i=2; i<=$NODE_NUM; i++)); do
   docker inspect --format '{{ .NetworkSettings.IPAddress }}' plda-node-$i >> hosts
 done
 
+echo "Building..."
+docker exec plda-master bash -c "cv /root/plda && make"
+
 echo "Training..."
-docker exec plda-master bash -c "cd /root/plda && make && time mpiexec -f ./docker/hosts -n $NODE_NUM ./mpi_lda --num_topics 2 --alpha 0.1 --beta 0.01 --training_data_file testdata/test_data.txt --model_file testdata/lda_model.txt --total_iterations 150"
+docker exec plda-master bash -c "time mpiexec -f ./docker/hosts -n $NODE_NUM ./mpi_lda --num_topics 2 --alpha 0.1 --beta 0.01 --training_data_file testdata/test_data.txt --model_file testdata/lda_model.txt --total_iterations 150"
 echo "Finished training."
 
 echo "Stop and remove containers..."
-docker stop `docker ps -n=$NODE_NUM -q`
-docker rm -v `docker ps -n=$NODE_NUM -q`
+docker stop plda-master
+docker rm plda-master
+for((i=2; i<=$NODE_NUM; i++)); do
+  docker stop plda-node-$i
+  docker rm plda-node-$i
+done
 echo "Finished."
