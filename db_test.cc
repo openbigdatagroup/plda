2f51b25a4825532c43e98ec9aba7d578a23db648
Donguk Lim <donguklim@ascentnet.co.jp>
2018-02-09 15:47:54 +0900

    fixed errors

--- db_test.cc
+++ db_test.cc
@@ -10,16 +10,15 @@ using namespace pqxx;
 
 int main(int argc, char* argv[]) {
     string const base_topic_req_sql = "SELECT target_url_is_included, target_url from topic_modeling_request "
-            "where pk=? and status=?";
-    string const base_req_lda_data_sql = "SELECT * from topic_modeling_lda_data where topic_request=?";
-    string const base_req_update_sql = "UPDATE * topic_modeling_request set status = 6 where pk=?";
-    string const base_page_data_sql = "SELECT word_dict from topic_modeling_url_page_data where topic_request=?";
+            "where id =  and status = ";
+    string const base_req_lda_data_sql = "SELECT * from topic_modeling_lda_data where topic_request = ";
+    string const base_req_update_sql = "UPDATE * topic_modeling_request set status = 6 where id = ";
+    string const base_page_data_sql = "SELECT word_dict from topic_modeling_url_page_data where topic_request = ";
     string sql;
     int pk = 118;
     unsigned long pos;
     try {
-        connection C("dbname = lm_backend user = lm_admin password = 1qazxsw2 \
-                             ostaddr = 127.0.0.1 port = 5432");
+        connection C("dbname = lm_backend user = lm_admin password = 1qazxsw2 hostaddr = 127.0.0.1 port = 5432");
         if (C.is_open()) {
             cout << "Opened database successfully: " << C.dbname() << endl;
         } else {
@@ -31,9 +30,9 @@ int main(int argc, char* argv[]) {
 
         /* Create SQL statement */
         sql = base_topic_req_sql;
-        pos = sql.find('?');
+        pos = sql.find('=') + 2;
         sql.insert(pos, std::to_string(pk));
-        pos = sql.find('?');
+        pos = sql.find('=', pos + 1) + 2;
         sql.insert(pos, std::to_string(STATUS_LDA_ANALYSIS));
 
         /* Execute SQL query */
@@ -52,12 +51,12 @@ int main(int argc, char* argv[]) {
 
         if (!request_exits){
             C.disconnect();
-            cout << "request " << pk << " does not exist" << endl;
+            cout << "uncompleted request " << pk << " does not exist" << endl;
             return 1;
         }
 
         sql = base_req_lda_data_sql;
-        pos = sql.find('?');
+        pos = sql.find('?') + 2;
         sql.insert(pos, std::to_string(pk));
         result lda_data( N.exec( sql ));
 
@@ -68,7 +67,7 @@ int main(int argc, char* argv[]) {
         for (result::const_iterator c = lda_data.begin(); c != lda_data.end(); ++c) {
             /* Create  SQL UPDATE statement */
             sql = base_req_update_sql;
-            pos = sql.find('?');
+            pos = sql.find('=') + 2;
             sql.insert(pos, std::to_string(pk));
             /* Execute SQL query */
             W.exec( sql );
@@ -78,7 +77,7 @@ int main(int argc, char* argv[]) {
         }
 
         sql = base_page_data_sql;
-        pos = sql.find('?');
+        pos = sql.find('=') + 2;
         sql.insert(pos, std::to_string(pk));
         result page_data( N.exec( sql ));
         for (result::const_iterator c = page_data.begin(); c != page_data.end(); ++c) {
