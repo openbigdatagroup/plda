9a513a9770259087eb46b9c8cb4124e67f1b21d0
Donguk Lim <donguklim@ascentnet.co.jp>
2018-02-14 10:23:00 +0900

    fixed wrong file name

--- docker/run_db_test_docker.sh
+++ docker/run_db_test_docker.sh
@@ -25,7 +25,7 @@ docker exec plda-master bash -c "cd /root/plda && make clean && make db_test"
 echo "Training..."
 # docker exec plda-master bash -c "time mpiexec -f ./docker/hosts -n $NODE_NUM ./db_test_mpi_lda --pk 118 --num_topics 2 --alpha 0.1 --beta 0.01 --training_data_file testdata/test_data.txt --model_file testdata/lda_model.txt --total_iterations 150"
 echo "Finished training."
-# docker exec plda-master bash -c "./db_test_lda"
+docker exec plda-master bash -c "./db_test"
 
 docker cp plda-master:/root/plda/testdata/lda_model.txt .
 
