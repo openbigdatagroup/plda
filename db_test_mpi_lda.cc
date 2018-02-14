2b2599525752e38e1a960c57f38275dfe626f9bf
Donguk Lim <donguklim@ascentnet.co.jp>
2018-02-14 18:56:46 +0900

    separated some works only for the main process

--- db_test_mpi_lda.cc
+++ db_test_mpi_lda.cc
@@ -140,13 +140,15 @@ namespace learning_lda {
     };
 
     int DistributelyLoadAndInitTrainingCorpus(
+            connection &C,
             int pk,
+            bool target_url_is_included,
+            string &target_url,
+            int min_word_length,
+            bool get_english,
             int num_topics,
             int myid, int pnum, LDACorpus* corpus, set<string>* words) {
-        string const base_topic_req_sql = "SELECT target_url_is_included, target_url, min_word_length, get_english "
-                "from topic_modeling_request where id = ?  and status = ?";
-        string const base_req_lda_data_sql = "SELECT * from topic_modeling_lda_data where topic_request_id = ?";
-        string const base_req_update_sql = "UPDATE topic_modeling_request set status = 6 where id = ?";
+
         string const base_page_topic_rel_sql = "SELECT pagedata_id from topic_modeling_url_page_data_topic_request "
                 "where topicmodelingrequest_id = ?";
         string const base_page_sql = "SELECT word_dict from topic_modeling_url_page_data where id = ?";
@@ -154,115 +156,15 @@ namespace learning_lda {
         unsigned long pos;
         regex_t reg;
         regcomp(&reg, ".*[a-z].*", REG_NOSUB | REG_EXTENDED);
-        string const base_connection_str = "dbname = lm_backend user = lm_admin password = 1qazxsw2 host = ? "
-                "port = 5432";
-        string connection_str = base_connection_str;
 
         corpus->clear();
         int index = 0;
 
-        const char* django_setting = std::getenv("DJANGO_SETTINGS_MODULE");
-        pos = connection_str.find('?');
-
-        if (django_setting == NULL){
-            connection_str.replace(pos, 1, "docker.for.mac.host.internal");
-        }
-        else if (strcmp(django_setting, "lm_backend.settings_stage")){
-            connection_str.replace(pos, 1, "192.168.7.50");
-        }
-        else if (strcmp(django_setting, "lm_backend.settings_prod")){
-            connection_str.replace(pos, 1, "192.168.7.19");
-        }
-        else if (strcmp(django_setting, "docker")){
-            connection_str.replace(pos, 1, "docker.for.mac.host.internal");
-        }
-        else{
-            connection_str.replace(pos, 1, "127.0.0.1");
-        }
-
-        connection C(connection_str);
-        if (C.is_open()) {
-            if(myid == 0){
-                std::cout << "Opened database successfully: " << C.dbname() << std::endl;
-            }
-        } else {
-            if(myid == 0){
-                std::cout << "Can't open database" << std::endl;
-            }
-            return 1;
-        }
-        /* Create a non-transactional object. */
-        nontransaction N(C);
-
-        /* Create SQL statement */
-        sql = base_topic_req_sql;
-        pos = sql.find('?');
-        sql.replace(pos, 1, to_string(pk));
-        pos = sql.find('?');
-        sql.replace(pos, 1, to_string(STATUS_LDA_ANALYSIS));
-
-        /* Execute SQL query */
-        result requests( N.exec( sql ));
-
-        /* List down all the records */
-        bool request_exits = false;
-        bool target_url_is_included;
-        string target_url;
-        int min_word_length;
-        bool get_english;
-        for (result::const_iterator c = requests.begin(); c != requests.end(); ++c) {
-            request_exits = true;
-            target_url_is_included = c[0].as<bool>();
-            if(!c[1].is_null()){
-                target_url = c[1].as<string>();
-            }
-            min_word_length = c[2].as<int>();
-            get_english = c[3].as<bool>();
-            break;
-        }
-
-        if (!request_exits){
-            C.disconnect();
-            if (myid == 0){
-                std::cout << "uncompleted request " << pk << " does not exist" << std::endl;
-            }
-            return 1;
-        }
-
-        sql = base_req_lda_data_sql;
-        pos = sql.find('?');
-        sql.replace(pos, 1, to_string(pk));
-        result lda_data( N.exec( sql ));
-        N.commit();
-
-        /* Create a transactional object. */
-
-        if (false && myid ==0){
-
-
-            work W(C);
-
-            /* if lda result exists, it means it is completed */
-            for (result::const_iterator c = lda_data.begin(); c != lda_data.end(); ++c) {
-                /* Create  SQL UPDATE statement */
-                sql = base_req_update_sql;
-                pos = sql.find('?');
-                sql.replace(pos, 1, to_string(pk));
-                /* Execute SQL query */
-                W.exec( sql );
-                W.commit();
-                C.disconnect ();
-                std::cout << "request " << pk << " is already completed" << std::endl;
-                return 1;
-            }
-
-        }
-
         sql = base_page_topic_rel_sql;
         pos = sql.find('?');
         sql.replace(pos, 1, to_string(pk));
-        nontransaction N2(C);
-        result page_topic_rel( N2.exec( sql ));
+        nontransaction N(C);
+        result page_topic_rel( N.exec( sql ));
         string word_dict;
 
         for (result::const_iterator c = page_topic_rel.begin(); c != page_topic_rel.end(); ++c) {
@@ -270,7 +172,7 @@ namespace learning_lda {
             sql = base_page_sql;
             pos = sql.find('?');
             sql.replace(pos, 1, to_string(page_id));
-            result page_data(N2.exec(sql));
+            result page_data(N.exec(sql));
             for (result::const_iterator c2 = page_data.begin(); c2 != page_data.end(); ++c2) {
                 string word_dict = c2[0].as<string>();
                 istringstream ss(word_dict);
@@ -341,7 +243,6 @@ namespace learning_lda {
                 index++;
             }
         }
-        C.disconnect ();
         return corpus->size();
     }
 
@@ -364,16 +265,60 @@ int main(int argc, char** argv) {
     using learning_lda::DistributelyLoadAndInitTrainingCorpus;
     using learning_lda::LDACmdLineFlags;
     int myid, pnum;
+
+    int pks[3] = {118, 117, 116};
+    string const base_topic_req_sql = "SELECT target_url_is_included, target_url, min_word_length, get_english "
+            "from topic_modeling_request where id = ?  and status = ?";
+    string const base_req_lda_data_sql = "SELECT * from topic_modeling_lda_data where topic_request_id = ?";
+    string const base_req_update_sql = "UPDATE topic_modeling_request set status = 6 where id = ?";
+    string sql;
+
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     MPI_Comm_size(MPI_COMM_WORLD, &pnum);
 
-    int pk = 118;
-    for (int i = 1; i < argc; ++i) {
-        if (0 == strcmp(argv[i], "--pk")) {
-            std::istringstream(argv[i + 1]) >> pk;
-            ++i;
+    int pk;
+    bool request_exits = false;
+    bool target_url_is_included;
+    string target_url;
+    int min_word_length;
+    bool get_english;
+
+    int num_val_buffer[3];
+    char url_buffer[2048];
+
+    int pos;
+    string connection_str = "dbname = lm_backend user = lm_admin password = 1qazxsw2 host = ? "
+            "port = 5432";
+    const char* django_setting = std::getenv("DJANGO_SETTINGS_MODULE");
+    pos = connection_str.find('?');
+
+    if (django_setting == NULL){
+        connection_str.replace(pos, 1, "127.0.0.1");
+    }
+    else if (strcmp(django_setting, "lm_backend.settings_stage")){
+        connection_str.replace(pos, 1, "192.168.7.50");
+    }
+    else if (strcmp(django_setting, "lm_backend.settings_prod")){
+        connection_str.replace(pos, 1, "192.168.7.19");
+    }
+    else if (strcmp(django_setting, "docker")){
+        connection_str.replace(pos, 1, "docker.for.mac.host.internal");
+    }
+    else{
+        connection_str.replace(pos, 1, "127.0.0.1");
+    }
+    connection C(connection_str);
+    if (C.is_open()) {
+        if(myid == 0){
+            std::cout << "Opened database successfully: " << C.dbname() << std::endl;
         }
+    } else {
+        if(myid == 0){
+            std::cout << "Can't open database" << std::endl;
+        }
+        return 1;
     }
 
     LDACmdLineFlags flags;
@@ -382,11 +327,107 @@ int main(int argc, char** argv) {
         return -1;
     }
 
+    if (myid == 0){
+        /* Create a non-transactional object. */
+        nontransaction N(C);
+        pk = pks[0];
+
+        /* Create SQL statement */
+        sql = base_topic_req_sql;
+        pos = sql.find('?');
+        sql.replace(pos, 1, to_string(pk));
+        pos = sql.find('?');
+        sql.replace(pos, 1, to_string(STATUS_LDA_ANALYSIS));
+
+        /* Execute SQL query */
+        result requests( N.exec( sql ));
+
+        /* List down all the records */
+        for (result::const_iterator c = requests.begin(); c != requests.end(); ++c) {
+            request_exits = true;
+            target_url_is_included = c[0].as<bool>();
+            if(!c[1].is_null()){
+                target_url = c[1].as<string>();
+            }
+            min_word_length = c[2].as<int>();
+            get_english = c[3].as<bool>();
+            break;
+        }
+
+        if (!request_exits){
+            if (myid == 0){
+                std::cout << "uncompleted request " << pk << " does not exist" << std::endl;
+            }
+            //return 1;
+        }
+
+        sql = base_req_lda_data_sql;
+        pos = sql.find('?');
+        sql.replace(pos, 1, to_string(pk));
+        result lda_data( N.exec( sql ));
+        N.commit();
+
+        /* Create a transactional object. */
+
+        if (false){
+
+
+            work W(C);
+
+            /* if lda result exists, it means it is completed */
+            for (result::const_iterator c = lda_data.begin(); c != lda_data.end(); ++c) {
+                /* Create  SQL UPDATE statement */
+                sql = base_req_update_sql;
+                pos = sql.find('?');
+                sql.replace(pos, 1, to_string(pk));
+                /* Execute SQL query */
+                W.exec( sql );
+                W.commit();
+                std::cout << "request " << pk << " is already completed" << std::endl;
+                // return 1;
+            }
+
+        }
+        num_val_buffer[0] = pk;
+        num_val_buffer[1] = min_word_length;
+        if(target_url_is_included){
+            num_val_buffer[2] = 1;
+        }
+        else{
+            num_val_buffer[2] = 0;
+        }
+        for (int process_id = 1; process_id < pnum; ++process_id){
+            MPI_Send(num_val_buffer, 3, MPI_INT, process_id, 0, MPI_COMM_WORLD);
+
+            int copy_length = target_url.size();
+            if (copy_length > 2048)
+                copy_length = 2048;
+            strncpy(url_buffer, target_url.c_str(), copy_length);
+            MPI_Send(target_url.c_str(), target_url.size(), MPI_CHAR, process_id, 1, MPI_COMM_WORLD);
+        }
+
+    }
+    else{
+        MPI_Recv(num_val_buffer, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(url_buffer, 2048, MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        pk = num_val_buffer[0];
+        min_word_length = num_val_buffer[1];
+        if(num_val_buffer[2] == 0){
+            target_url_is_included = false;
+        }
+        else{
+            target_url_is_included = true;
+        }
+
+        target_url = string(url_buffer);
+    }
+
     srand(time(NULL));
 
     LDACorpus corpus;
     set<string> allwords;
-    CHECK_GT(DistributelyLoadAndInitTrainingCorpus(pk,
+    CHECK_GT(DistributelyLoadAndInitTrainingCorpus(C, pk, target_url_is_included, target_url, min_word_length,
+                                                   get_english,
                                                    flags.num_topics_,
                                                    myid, pnum, &corpus, &allwords), 0);
     std::cout << "Training data loaded" << std::endl;
@@ -436,6 +477,7 @@ int main(int argc, char** argv) {
         model.AppendAsString(fout);
     }
     FreeCorpus(&corpus);
+    C.disconnect();
     MPI_Finalize();
     return 0;
 }
