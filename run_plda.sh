ca1d7b5d2dd1ba668e418d87a495c134b69832dc
Donguk Lim <donguklim@ascentnet.co.jp>
2018-02-20 16:27:57 +0900

    diable debugging logs

--- run_plda.sh
+++ run_plda.sh
@@ -11,8 +11,8 @@ check_process() {
 
 ts=`date +%T`
 
-echo "$ts: Django setting: $DJANGO_SETTINGS_MODULE"
-echo -n "$ts: mls02 ip is " && ssh -G mls02 | awk '/^hostname / { print $2 }'
+# echo "$ts: Django setting: $DJANGO_SETTINGS_MODULE"
+# echo -n "$ts: mls02 ip is " && ssh -G mls02 | awk '/^hostname / { print $2 }'
 echo "$ts: check if lm_plda is running"
 check_process "lm_plda"
 [ $? -eq 0 ] && echo "$ts: not running, restarting..." && mpiexec -f /home/dev/plda/lm_hosts -n 17 /home/dev/plda/lm_plda
