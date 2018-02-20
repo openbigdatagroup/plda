f4c3b307f1b67c63e91d311ec4f47dfb24b495fc
Donguk Lim <donguklim@ascentnet.co.jp>
2018-02-20 09:58:52 +0900

    do not daemonize

--- lm_plda.cc
+++ lm_plda.cc
@@ -49,13 +49,6 @@
 #include "sampler.h"
 #include "cmd_flags.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-//#include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
 #include <iostream>
 #include <fstream>
 
@@ -507,60 +500,6 @@ void lda_redis_unlock(cpp_redis::client& client, const string& token){
 }
 
 
-static void skeleton_daemon()
-{
-    pid_t pid;
-
-    /* Fork off the parent process */
-    pid = fork();
-
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-    {
-        std::cout << "parent exit" << std::endl;
-        exit(EXIT_SUCCESS);
-    }
-
-    /* On success: The child process becomes session leader */
-    if (setsid() < 0)
-        exit(EXIT_FAILURE);
-
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-
-    /* Fork off for the second time*/
-    pid = fork();
-
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-
-    /* Set new file permissions */
-    umask(0);
-
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    //chdir("/root/plda");
-
-    /* Close all open file descriptors */
-    int x;
-    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
-    {
-        close (x);
-    }
-
-
-}
 
 
 int main(int argc, char** argv) {
@@ -571,9 +510,6 @@ int main(int argc, char** argv) {
     using learning_lda::DistributelyLoadAndInitTrainingCorpus;
     using learning_lda::LDACmdLineFlags;
     using learning_lda::save_to_lm;
-
-
-    skeleton_daemon();
 
     /* Open the log file */
     std::ofstream out("/var/log/plda_daemon.log");
